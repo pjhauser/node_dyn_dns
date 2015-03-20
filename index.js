94552df064356bebc6aeac823dbed4d5d04030cf
Phil Hauser <phil.hsr@gmail.com>
2015-03-20 15:24:35 +0000

    adding conf tpl and nginx cmd

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 var express = require('express');
+var _ = require('underscore');
 var fs = require("fs");
+var exec = require("child_process");
 
 var app = express();
 app.enable('trust proxy');
 
 var domain = "home.pjhauser.com"
-
-
-var config = function(ip){
-	var confString = "server {\
-		listen 80;\
-		server_name " + domain + ";\
-		location / {\
-			proxy_pass http://"+ ip +"/;\
-			proxy_http_version 1.1;\
-			proxy_set_header Upgrade $http_upgrade;\
-			proxy_set_header Connection 'upgrade';\
-			proxy_set_header Host $host;\
-			proxy_cache_bypass $http_upgrade;\
-		}\
-	}"
-	return confString;
-};
+var domainTemplate = "home.domain.com.conf.tpl"
 
 app.get('/:clientID', function(req, res) {
 	var clientID = req.params.clientID;
 	var ip = req.ip;
 
 	if(clientID == "123"){
-		var configFile = config(ip);
-		var fileName = domain + ".conf";
-		fs.writeFile(fileName, configFile, function(err) {
-			if(err) {
+
+
+		fs.readFile(domainTemplate, "utf8",function(err, data){
+			if(err){
 				return console.log(err);
 			}
-		}); 
+
+			var tpl = _.template(data);
+			var configFile = tpl({ip: ip, domain: domain});
+
+			var fileName = domain + ".conf";
+			fs.writeFile(fileName, configFile, function(err) {
+				if(err) {
+					return console.log(err);
+				}
+			}); 
+
+			exec("sudo service nginx reload");
+
+		});
+
 	}
 
 	res.send(ip);
